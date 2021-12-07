b1b00827be6637e6290dfb39c5e823d9e72941be
devmemory <apsapo12@gmail.com>
2021-12-07 17:29:46 +0900

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,6 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = function (app) {
-    console.log('called')
     app.use(
         createProxyMiddleware('/api',{
             target: 'http://localhost:8080/',
